1a9823045fa38fec04f8bf0326653646805f647b
emmanuel rodriguez encarnacion <emmanuel_rodriguezencarnacion2@hotmail.com>
2022-04-16 15:57:38 -0400

    V0.003

---
+++ capa_datos/ExistenciasD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using capa_entidad;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_datos
+{
+    public class ExistenciasD
+    {
+        private readonly todayEntities3 db = new todayEntities3();
+
+        public List<existenciasXalmacen> List_Existencias()
+        {
+            return db.existenciasXalmacen.ToList();
+        }
+
+        public void Add_Existencia(existenciasXalmacen ext)
+        {
+            db.existenciasXalmacen.Add(ext);
+            db.SaveChanges();
+        }
+        public void Drop_Existencia(existenciasXalmacen ext)
+        {
+            var Eentidad = db.existenciasXalmacen.FirstOrDefault(a => a.IDarticulo == ext.IDarticulo);
+
+            db.existenciasXalmacen.Remove(Eentidad);
+            db.SaveChanges();
+        }
+    }
+}
